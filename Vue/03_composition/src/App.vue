1fbdf5d920d7209cba12e1f1e6fd7bd9fe1e8857
sangwon <chosw0410@naver.com>
2025-09-16 12:50:06 +0900

    Calc1

---
+++ Vue/03_composition/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2>Composition API</h2>
+    <h3>1. 반응성을 가진 상태 데이터</h3>
+
+    <calc1/>
+  </div>
+</template>
+
+<script>
+import Calc1 from './components/Calc1.vue';
+export default {
+  setup () {
+    
+
+    return {}
+  },
+  components:{
+    Calc1
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ Vue/03_composition/src/components/Calc1.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <label for="num1">첫 번째 수 : </label>
+        <input type="text" id="num1" v-model.number="num1">
+
+        <br><br>
+
+        <label for="num1">두 번째 수 : </label>
+        <input type="text" id="num1" v-model.number="num2"></input>
+
+        <br></br>
+        <button>계산하기</button>
+        <br></br>
+        <p>
+            결과 : {{ result }}
+        </p>
+    </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+    setup () {
+        const num1 = ref(10);
+        const num2 = ref(20);
+        const result = ref(30);
+
+        return {
+            num1,
+            num2,
+            result
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
